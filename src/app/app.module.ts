692da50aab9cc3c625aaa8631eb914631b52b614
Yi-Coder <toy041126@gmail.com>
2018-09-05 21:18:18 -0400

    minor
    minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,74 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
-import {
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-  AccordionModule,
-} from '@angular/material';
-
-
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { PostsComponent } from './posts/posts.component';
-import { UsersComponent } from './users/users.component';
-import { DetailsComponent } from './details/details.component';
-
+import {SpecificationComponent} from './specification/specification.component';
+import {AppComponent} from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    PostsComponent,
-    UsersComponent,
-    DetailsComponent
+    SpecificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/specification/specification.component.ts
+++ src/app/specification/specification.component.ts
@@ -1,49 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {Specification} from '../model/Specification';
 import {FormControl} from '@angular/forms';
+import { MatSelectModule } from '@angular/material';
 
-import {MultiSelectModule} from 'primeng/multiselect';
-
-import {SelectItem} from 'primeng/api';
-
-interface City {
-    name: string,
-    code: string
-}
+//import {MultiSelectModule} from 'primeng/multiselect';
+//import {SelectItem} from 'primeng/api';
 
 @Component({
   selector: 'app-specification',
   templateUrl: './specification.component.html',
   styleUrls: ['./specification.component.css']
 })
-
 export class SpecificationComponent implements OnInit {
-     cities1: SelectItem[];
 
-     cities2: City[];
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+     ngOnInit(){
 
-     selectedCities1: City[];
-
-     selectedCities2: City[];
-
-     constructor() {
-         //SelectItem API with label-value pairs
-         this.cities1 = [
-             {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
-             {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
-             {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
-             {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}}
-             {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
-         ];
-
-         //An array of cities
-         this.cities2 = [
-             {name: 'New York', code: 'NY'},
-             {name: 'Rome', code: 'RM'},
-             {name: 'London', code: 'LDN'},
-             {name: 'Istanbul', code: 'IST'},
-             {name: 'Paris', code: 'PRS'}
-         ];
      }
 
 }
